11f8db93fa8d9d730c00acb051b85a53d78b7d54
blkr4 <36671962+blkr4@users.noreply.github.com>
2025-02-22 11:49:39 +0000

    patient routes

--- src/routes/patient.route.js
+++ src/routes/patient.route.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import { getPatientById, getAllPatients, updatePatient, createPatient } from '../modules/patient/patient.controller.js';
-import { validate } from '../middlewares/validators.js';
 import authenticate from '../middlewares/authorization.js';
 
 const patientRouter = express.Router()
 
-patientRouter.get('/:id', authenticate, validate(), getPatientById);
-patientRouter.get('/',authenticate, getAllPatients);
-patientRouter.put('/:id',authenticate, updatePatient);
-patientRouter.post('/',authenticate, createPatient);
+patientRouter.get('/:id', authenticate, getPatientById);
+patientRouter.get('/', authenticate, getAllPatients);
+patientRouter.put('/:id', authenticate, updatePatient);
+patientRouter.post('/', authenticate, createPatient);
 
 export default patientRouter
